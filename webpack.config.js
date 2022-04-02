e72c2a6619c2f07750aba2719d7017d591595df2
atgrafic <troczynska.anna@gmail.com>
2022-04-02 21:38:50 +0200

    start

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: { index: "./src/index.js" },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[contenthash].bundle.js",
+        clean: true,
+    },
+    devServer: {
+        static: path.join(__dirname, "dist"),
+        port: 9000,
+        hot: true,
+        liveReload: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new CopyPlugin({
+            patterns: [{ from: "src/asset/image", to: "./image" }],
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: {
+                    loader: "file-loader",
+                    options: {
+                        outputPath: "./image",
+                        name: "[name].[ext]",
+                    },
+                },
+            },
+            {
+                test: /\.(html)$/,
+                use: ["html-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+
+                    },
+                },
+            },
+        ],
+    },
+};
