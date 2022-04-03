074c98626856f50742c0a465bb125d6db1755d12
atgrafic <troczynska.anna@gmail.com>
2022-04-03 13:04:20 +0200

    back

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 "use strict";
 
-import normalize from "./css/normalize.scss";
-import style from "./css/index.scss";
-import header_style from "./css/header.scss";
 
-import header from "./views/header.js";
 
 console.log("Hello World");
