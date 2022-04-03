326170a948d2d3f06a38399c75260178599d2a67
atgrafic <troczynska.anna@gmail.com>
2022-04-03 20:31:38 +0200

    nav-img

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require("path");
 
 const webpack = require("webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
+//const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
     entry: { index: "./src/index.js" },
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[contenthash].bundle.js",
+        assetModuleFilename: 'images/[name][ext][query]',
         clean: true,
     },
     devServer: {
@@ -21,9 +22,9 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: "./src/index.html",
         }),
-        new CopyPlugin({
-            patterns: [{ from: "src/asset/image", to: "./asset/image" }],
-        }),
+        // new CopyPlugin({
+        //     patterns: [{ from: "src/assets/image", to: "./image" }],
+        // }),
     ],
     module: {
         rules: [
@@ -31,28 +32,32 @@ module.exports = {
                 test: /\.scss$/,
                 use: ["style-loader", "css-loader", "sass-loader"],
             },
+            // {
+            //     test: /\.(png|svg|jpg|gif)$/,
+            //     use: {
+            //         loader: 'url-loader',
+            //       },
+
+            // },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                use: {
-                    loader: "file-loader",
-                    options: {
-                        outputPath: "./image",
-                        name: "[name].[ext]",
-                    }
-                },
-            },
+                type: 'asset/resource'
+              },
             {
                 test: /\.(html)$/,
                 use: ["html-loader"],
             },
             {
-                test: /\.js$/,
-                exclude: /node_modules/,
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: "babel-loader",
                     options: {
                         presets: ["@babel/preset-env"],
-
+                        plugins: [
+                            "@babel/plugin-proposal-class-properties",
+                            "@babel/plugin-transform-runtime",
+                        ],
                     },
                 },
             },
