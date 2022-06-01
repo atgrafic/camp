7a9c8a551bfce5b715a9bdedb1ae655f683a9b9b
atgrafic <troczynska.anna@gmail.com>
2022-06-01 20:37:21 +0200

    camp_choose

--- src/views/chooseCamp.js
+++ src/views/chooseCamp.js
@@ -9,7 +9,7 @@ export default class extends view {
     }
 
     async getHtml() {
-        return (`
+        return `
  <div class="bg_breadcrumb">
         <div class="small_menu"></div>
         <div class="breadcrumb_menu">
@@ -19,6 +19,56 @@ export default class extends view {
             </ul>
         </div>
     </div>
-    `);
+    <!--4 buttony i propozycja-->
+    <div class="main_bg">
+    <div class="main_position">
+    <div class="main_postion_left">
+    <div class="choose_button">
+    <div class="camp_all">
+    <a href="/#" data-link>
+    <h2 class="choose_button_all">Wszystkie <br/> obozy i kolonie</h2>
+    </a>
+    </div>
+    <div class="camp_abroad">
+    <a href="/#" data-link>
+    <h2 class="choose_button_abroad">Obozy <br/> zagraniczne</h2>
+    </a>
+    </div>
+    <div class="camp_sea">
+    <a href="/#" data-link>
+    <h2 class="choose_button_sea">Kolonie <br/> nad morzem</h2>
+    </a>
+    </div>
+    <div class="camp_mountains">
+    <a href="/#" data-link>
+    <h2 class="choose_button_mountains">Klonie <br/> w górach</h2>
+    </a>
+    </div>
+    </div>
+    <!--lista koloni-->
+    <!--paginacja-->
+
+
+    </div>
+    <div class="main_postion_right">
+    <div class="camp_subject">
+    <h2>Kolonie i Obozy tematyczne</h2>
+    <div class="list_sub">
+    <li>Akademia Relaksu</li>
+    <li>Akademia Tańca</li>
+    <li>Akademia Malarstwa</li>
+    <li>Akademia Wokalu</li>
+    <li>Akademia Sportu</li>
+    <li>kolonia Survivalowa</li>
+    <li>Kolonie 15 Przygód</li>
+    <li>Akademia Jazdy konnej</li>
+    <ul>
+    </ul>
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    `;
     }
 }
