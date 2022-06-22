b961bff571f5da636ed1277dd365ffaf7744eb10
atgrafic <troczynska.anna@gmail.com>
2022-06-22 21:54:02 +0200

    3 podstrona done

--- src/views/campList.js
+++ src/views/campList.js
@@ -44,23 +44,28 @@ function campListAll(campId) {
     for (let i = 0; i < camp.length; i++) {
         if (campId === camp[i].id) {
             return `<div class="main-box">
-                <div class="sumPhoto">
-                    <img id="carPhoto1" src="${camp[i].image[0]}">
-                    <img id="carPhoto2" src="${camp[i].image[1]}">
-                    <img id="carPhoto3" src="${camp[i].image[2]}">
-                    <img id="carPhoto4" src="${camp[i].image[3]}">
-                </div>
-                <div class="sum">
-                <div class="sumDescription">
-                <h1>${camp[i].city}</h1>
-                <p>${camp[i].name}</p>
-                <p>${camp[i].description}</p>
-                </div>
-                <div class="sumPrice">
-                <p class="price">${camp[i].price}</p>
-                </div>
-                </div>
-            </div>`;
+                        <div class="img-box">
+                            <div class="sumPhotoBig">
+                                <img id="expandedImg" src="${camp[i].image[0]}">
+                            </div>
+                            <div class="sumPhoto">
+                                <img id="campPhoto1" src="${camp[i].image[0]}">
+                                <img id="campPhoto2" src="${camp[i].image[1]}">
+                                <img id="campPhoto3" src="${camp[i].image[2]}">
+                                <img id="campPhoto4" src="${camp[i].image[3]}">
+                            </div>
+                        </div>
+                        <div class="sum">
+                            <div class="sumDescription">
+                                <h1>${camp[i].city}</h1>
+                                <p>${camp[i].name}</p>
+                                <p>${camp[i].description}</p>
+                            </div>
+                            <div class="sumPrice">
+                                <p class="price">${camp[i].price}</p>
+                            </div>
+                        </div>
+                    </div>`;
         }
     }
 }
@@ -69,7 +74,8 @@ function tabs(campId) {
     for (let i = 0; i < camp.length; i++) {
         if (campId === camp[i].id) {
             return `
-            <div class="tabs main_bg">
+            <div class="main_bg ">
+            <div class="tabs main_with">
     <input id="tab1" type="radio" name="tabs" checked>
     <label for="tab1">Program</label>
 
@@ -77,7 +83,7 @@ function tabs(campId) {
     <label for="tab2">Terminy i opis ośrodka</label>
 
     <input id="tab3" type="radio" name="tabs">
-    <label for="tab3">warunki rezerwacji</label>
+    <label for="tab3">Warunki rezerwacji</label>
 
     <section id="content_1">
     <h2>${camp[i].program_title}</h2>
@@ -100,11 +106,45 @@ function tabs(campId) {
     <h2>${camp[i].sleep_title_2}</h2>
     <p>${camp[i].sleep_description_2}</p>
     </section>
-    <section id="content3">
+    <section id="content_3">
      <h2>Rezerwacja terminu</h2>
      <p> W celu rezerwacji prosimy o kontant telefoniczny</p>
     </section>
+    </div>
     </div> `;
         }
     }
+}
+
+
+document.addEventListener("click", imageEvent);
+
+function imageEvent() {
+    let expandImg = document.getElementById("expandedImg");
+
+    let campPhoto1 = document.getElementById("campPhoto1");
+    let campPhoto1Src = campPhoto1.src;
+
+    let campPhoto2 = document.getElementById("campPhoto2");
+    let campPhoto2Src = campPhoto2.src;
+
+    let campPhoto3 = document.getElementById("campPhoto3");
+    let campPhoto3Src = campPhoto3.src;
+
+    let campPhoto4 = document.getElementById("campPhoto4");
+    let campPhoto4Src = campPhoto4.src;
+
+    campPhoto1.addEventListener("click", function () {
+        expandImg.src = campPhoto1Src;
+    });
+    campPhoto2.addEventListener("click", function () {
+        expandImg.src = campPhoto2Src;
+    });
+
+    campPhoto3.addEventListener("click", function () {
+        expandImg.src = campPhoto3Src;
+    });
+    campPhoto4.addEventListener("click", function () {
+        expandImg.src = campPhoto4Src;
+    });
 }
